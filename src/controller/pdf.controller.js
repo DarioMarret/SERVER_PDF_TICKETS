62380f1741677b70ecf268f92adcceca71b44fb4
DarioMarret <javier_dario_marret@hotmail.com>
2022-08-27 15:03:35 -0500

    first commit

--- src/controller/pdf.controller.js
+++ src/controller/pdf.controller.js
@@ -1,19 +1,23 @@
 import { base64encode, base64decode } from 'nodejs-base64'
 import { conexion } from "../database/Conexion"
 var html_to_pdf = require('html-pdf-node')
-import 'dotenv/config'
+import NanoDate from "nano-date"
+import moment from "moment"
 import path from 'path'
+import 'dotenv/config'
 import fs from 'fs'
+moment.locale("es")
 
 
 export const GenaradorPdrQR = async (req, reply) => {
+    var nano = Number(new NanoDate(moment().format("YYYY-MM-DD HH:mm:ss")).getTime())
 
     let options = { format: 'A4' };
     let file = { url: `https://rec.netbot.ec/pdf/${base64encode(JSON.stringify(req.body))}` };
     html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
-        fs.writeFileSync(path.join(__dirname, `../public/tickets/${req.body.protocol}.pdf`), pdfBuffer);
+        fs.writeFileSync(path.join(__dirname, `../public/tickets/${req.body.protocol}-${req.body.protocol}-${nano}.pdf`), pdfBuffer);
     });
-    const link = `${process.env.DOMINIO}/tickets/${req.body.protocol}.pdf`
+    const link = `${process.env.DOMINIO}/tickets/${req.body.protocol}-${nano}.pdf`
     const qr = `${req.body.nombre}-${req.body.cedula}-${req.body.celular}-${req.body.protocol}-${req.body.actual}`
     await GuardarDatos(req.body.nombre, req.body.cedula, req.body.celular, req.body.protocol, req.body.actual, qr, link)
     reply.send({
