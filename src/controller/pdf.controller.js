5c6c0cfc6cb24200fa5f82f45726b98cbdb3bade
DarioMarret <javier_dario_marret@hotmail.com>
2022-09-05 16:30:53 -0500

    sqlite

--- src/controller/pdf.controller.js
+++ src/controller/pdf.controller.js
@@ -13,27 +13,46 @@ moment.locale("es")
 export const GenaradorPdrQR = async (req, reply) => {
     var nano = Number(new NanoDate(moment().format("YYYY-MM-DD HH:mm:ss")).getTime())
 
-    let options = { format: 'A4' };
-    let file = { url: `https://rec.netbot.ec/pdf/${base64encode(JSON.stringify(req.body))}` };
-    html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
-        fs.writeFileSync(path.join(__dirname, `../public/tickets/${req.body.protocol}-${nano}.pdf`), pdfBuffer);
-    });
-    const link = `${process.env.DOMINIO}/tickets/${req.body.protocol}-${nano}.pdf`
-    const qr = `${req.body.nombre}-${req.body.cedula}-${req.body.celular}-${req.body.protocol}-${req.body.actual}`
-    await GuardarDatos(req.body.nombre, req.body.cedula, req.body.celular, req.body.protocol, req.body.actual, qr, link, req.body.nombreconcert, req.body.cuidadconcert)
+    // let options = { format: 'A4' };
+    // let file = { url: `https://rec.netbot.ec/pdf/${base64encode(JSON.stringify(req.body))}` };
+    // html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
+    //     fs.writeFileSync(path.join(__dirname, `../public/tickets/${req.body.protocol}-${nano}.pdf`), pdfBuffer);
+    // });
+    const generadorQR = await GenerarPdfQR(req.body)
+    if (generadorQR) {
+        const link = `${process.env.DOMINIO}/tickets/${req.body.protocol}-${nano}.pdf`
+        const qr = `${req.body.nombre}-${req.body.cedula}-${req.body.celular}-${req.body.protocol}-${req.body.actual}`
+        await GuardarDatos(req.body.nombre, req.body.cedula, req.body.celular, req.body.protocol, req.body.actual, qr, link, req.body.nombreconcert, req.body.cuidadconcert, req.body)
+        const delay = time => new Promise(resolveCallback => setTimeout(resolveCallback, time));
+        await delay(3000)
+        reply.send({
+            success: true,
+            url: link
+        });
+    } else {
+        reply.send({
+            success: false,
+            url: ""
+        });
+    }
+}
 
-    const delay = time => new Promise(resolveCallback => setTimeout(resolveCallback, time));
-    await delay(5000)
-    
-    reply.send({
-        success: true,
-        url:link
-    });
+async function GenerarPdfQR(body) {
+    return new Promise((resolve, reject) => {
+        var nano = Number(new NanoDate(moment().format("YYYY-MM-DD HH:mm:ss")).getTime())
+        let options = { format: 'A4' };
+        let file = { url: `https://rec.netbot.ec/pdf/${base64encode(JSON.stringify(body))}` };
+        html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
+            fs.writeFileSync(path.join(__dirname, `../public/tickets/${body.protocol}-${nano}.pdf`), pdfBuffer);
+            resolve(true)
+        });
+    })
+
 }
 
 async function GuardarDatos(nombre, cedula, celular, protocol, actual, qr, link, nombreconcert, cuidadconcert) {
-    let query = `INSERT INTO generados_tickets (nombre, cedula, celular, protocol, actual, qr, link, nombreconcert, cuidadconcert) 
-    VALUES ('${nombre}', '${cedula}', '${celular}', '${protocol}', '${actual}', '${qr}', '${link}', '${nombreconcert}', '${cuidadconcert}')`;
+    let query = `INSERT INTO generados_tickets (nombre, cedula, celular, protocol, actual, qr, link, nombreconcert, cuidadconcert,body) 
+    VALUES ('${nombre}', '${cedula}', '${celular}', '${protocol}', '${actual}', '${qr}', '${link}', '${nombreconcert}', '${cuidadconcert}','${JSON.stringify(body)}')`;
     await conexion.query(query)
 }
 
@@ -45,6 +64,6 @@ export const consultarCedula = async (req, reply) => {
         reply.send({
             success: false,
             message: "Error al consultar cedula"
-        })        
+        })
     }
 }
